b5f362cbdc4d3944f844711de73601448f1e6776
msavastano <millhouse5000@gmail.com>
2015-01-03 20:18:02 -0500

    muslim holiday

--- app/src/main/java/com/mikesavastano/howlongtil/HolidayCalendar.java
+++ app/src/main/java/com/mikesavastano/howlongtil/HolidayCalendar.java
@@ -37,6 +37,7 @@ public class HolidayCalendar {
         holidays.add(Arrays.asList("2", "1", "4", "99", "Mother's Day (US)"));
         holidays.add(Arrays.asList("-1", "2", "4", "99", "Memorial Day (US)"));
         holidays.add(Arrays.asList("99", "99", "6", "4", "Independence Day (US)"));
+        holidays.add(Arrays.asList("99", "99", "99", "99", "Eid al-Fitr"));
         //Ramadan
         holidays.add(Arrays.asList("1", "2", "8", "99", "Labor Day (US)"));
         holidays.add(Arrays.asList("99", "99", "9", "31", "Halloween (US)"));
@@ -60,7 +61,9 @@ public class HolidayCalendar {
 
     public Calendar calcHoliday (Map<String, String> date, int nyear) {
         Calendar hol = Calendar.getInstance();
-        if(date.get("name")=="Easter (US)"){
+        if(date.get("name")=="Eid al-Fitr"){
+            return writeIslamicDate(nyear);
+        }else if(date.get("name")=="Easter (US)"){
             return EasterSunday(nyear);
         }else if(date.get("name")=="Good Friday (US)"){
             return GoodFriday(EasterSunday(nyear));
@@ -87,7 +90,7 @@ public class HolidayCalendar {
     }
 
 
-
+    //http://core0.staticworld.net/downloads/idge/imported/article/jvw/1998/01/holidays.java
     public static Calendar EasterSunday(int nYear)
     {
         Calendar easter = Calendar.getInstance();
@@ -130,25 +133,119 @@ public class HolidayCalendar {
         return gf;
     }
 
-    public String EidAlFitr(int nyear){
-        ULocale l = new ULocale("@calendar=islamic");
-
-        IslamicCalendar dtIslamic = new com.ibm.icu.util.IslamicCalendar(l);
-
-        dtIslamic.setCivil(true);
-
-
-
-
-        DateTimeFormatter fmt = DateTimeFormat.forPattern("MM, dd, yyyy");
-        DateFormat usDateFormat = new SimpleDateFormat("MM/dd/yyyy");
-       //System.out.println(dtIslamic);
-        //String str = fmt.print(dtIslamic.toString());
-        return usDateFormat.format(dtIslamic.getTime());
-    }
-
     public static void main(String [] args){
         HolidayCalendar h = new HolidayCalendar();
-        System.out.println(h.EidAlFitr(2016));
+        //System.out.println(h.EidAlFitr(2016));
+        System.out.println(writeIslamicDate(2018).getTime());
+    }
+
+    //http://www.coderanch.com/t/534271/java/java/Gregorian-Hijri-Dates-Converter-JAVA
+    static double gmod(double n,double  m) {
+        return ((n % m) + m) % m;
+    }
+
+    static double[] kuwaiticalendar(boolean adjust, int nyear) {
+        Calendar today = Calendar.getInstance();
+        int adj=0;
+        if(adjust){
+            adj=0;
+        }else{
+            adj=1;
+        }
+
+        if (adjust) {
+            int adjustmili = 1000 * 60 * 60 * 24 * adj;
+            long todaymili = today.getTimeInMillis() + adjustmili;
+            today.setTimeInMillis(todaymili);
+        }
+        double day = today.get(Calendar.DAY_OF_MONTH);
+        double  month = today.get(Calendar.MONTH);
+        double  year = nyear;
+
+        double  m = month + 1;
+        double  y = year;
+        if (m < 3) {
+            y -= 1;
+            m += 12;
+        }
+
+        double a = Math.floor(y / 100.);
+        double b = 2 - a + Math.floor(a / 4.);
+
+        if (y < 1583)
+            b = 0;
+        if (y == 1582) {
+            if (m > 10)
+                b = -10;
+            if (m == 10) {
+                b = 0;
+                if (day > 4)
+                    b = -10;
+            }
+        }
+
+        double jd = Math.floor(365.25 * (y + 4716)) + Math.floor(30.6001 * (m + 1)) + day
+                + b - 1524;
+
+        b = 0;
+        if (jd > 2299160) {
+            a = Math.floor((jd - 1867216.25) / 36524.25);
+            b = 1 + a - Math.floor(a / 4.);
+        }
+        double bb = jd + b + 1524;
+        double cc = Math.floor((bb - 122.1) / 365.25);
+        double dd = Math.floor(365.25 * cc);
+        double ee = Math.floor((bb - dd) / 30.6001);
+        day = (bb - dd) - Math.floor(30.6001 * ee);
+        month = ee - 1;
+        if (ee > 13) {
+            cc += 1;
+            month = ee - 13;
+        }
+        year = cc - 4716;
+
+        double wd = gmod(jd + 1, 7) + 1;
+
+        double iyear = 10631. / 30.;
+        double epochastro = 1948084;
+        double epochcivil = 1948085;
+
+        double shift1 = 8.01 / 60.;
+
+        double z = jd - epochastro;
+        double cyc = Math.floor(z / 10631.);
+        z = z - 10631 * cyc;
+        double j = Math.floor((z - shift1) / iyear);
+        double iy = 30 * cyc + j;
+        z = z - Math.floor(j * iyear + shift1);
+        double im = Math.floor((z + 28.5001) / 29.5);
+        if (im == 13)
+            im = 12;
+        double id = z - Math.floor(29.5001 * im - 29);
+
+        double[]  myRes = new double[8];
+
+        myRes[0] = day; // calculated day (CE)
+        myRes[1] = month - 1; // calculated month (CE)
+        myRes[2] = year; // calculated year (CE)
+        myRes[3] = jd - 1; // julian day number
+        myRes[4] = wd - 1; // weekday number
+        myRes[5] = id; // islamic date
+        myRes[6] = im - 1; // islamic month
+        myRes[7] = iy; // islamic year
+
+        return myRes;
+    }
+    static Calendar writeIslamicDate(int nyear) {
+
+        boolean dayTest = false;
+        double[] iDate = kuwaiticalendar(dayTest, nyear);
+
+        IslamicCalendar dtIslamic = new com.ibm.icu.util.IslamicCalendar((int)iDate[7],  9,  1);
+
+        Long milliTime = dtIslamic.getTimeInMillis(); //.getTime()+"  "+Double.toString(iDate[6])+ " "+Double.toString(iDate[5])+" " +Double.toString(iDate[7]);
+        Calendar tempc = Calendar.getInstance();
+        tempc.setTimeInMillis(milliTime);
+        return tempc;
     }
 }
